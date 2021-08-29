76be6f33b4d3ac1fd0f4db1e4d9c23704891e5be
Silas Jökel <silas@joekel.info>
2021-08-29 11:41:59 +0200

    refactor: simplify config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const Config = {
     "liga": "Nitro League Division 1",
     "format": "Bo7",
     "stingerDelay": 1000,
-    // Remove below to get Team names and series score from ROCS
+    /* Uncomment this section to override team names and series score from ROCS
     "blue": [
       "Team Blue",
       "0"
@@ -12,5 +12,6 @@ const Config = {
       "Team Orange",
       "0"
     ]
+    */
   }
 }
