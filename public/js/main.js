10dae3733467024550d3adb1d49692ac1466e7ee
Silas Jökel <silas@joekel.info>
2021-11-17 16:55:18 +0100

    fix: kph has no space to amount

--- public/js/main.js
+++ public/js/main.js
@@ -54,13 +54,9 @@ WsSubscribers.subscribe("game", "goal_scored", (d) => {
     $(".ballspeed").text(Math.round(d.goalspeed));
     $(".scorer").text(d.scorer.name);
     if (d.assister.name !== "") {
-        $(".assister-caption").show();
-        $(".assister").show();
         $(".assister").text(d.assister.name);
-    } else {
-        $(".assister-caption").hide();
-        $(".assister").hide();
-    }
+        $(".assister-span").show();
+    } else $(".assister-span").hide();
 
     if (Config.settings.playerCams.includes('goal')) {
         $(".player-cams").show();
